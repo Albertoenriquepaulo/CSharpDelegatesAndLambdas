1943bb8ab1ee8c0e94370093f6748d26d6b506e4
Alberto Paulo <Albertopaulo@gmail.com>
2021-03-16 11:57:41 +0100

    Generic Delegates with Generic Methods

--- DelegatesAndLambdas/Program.cs
+++ DelegatesAndLambdas/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Metadata;
+using System.Reflection.Metadata.Ecma335;
 
 var heroes = new List<Hero>
 {
@@ -11,18 +13,25 @@ var heroes = new List<Hero>
     new(string.Empty, string.Empty, "Storm-front", true),
 };
 
-var heroesWhoCanFly = HeroesFilterWithoutDelegate(heroes, (Hero hero) => hero.CanFly);
+// You can build the function using parenthesis or not
+var heroesWhoCanFly = HeroesFilterWithoutDelegate(heroes, hero => hero.CanFly);
 var heroesWhoCanFlyString = string.Join(", ", heroesWhoCanFly);
 Console.WriteLine("HEROES WHO CAN FLY");
 Console.WriteLine(heroesWhoCanFlyString);
 
+// You can build the function using parenthesis or not
 var heroesUnknownLastName = HeroesFilterWithDelegate(heroes, (Hero hero) => string.IsNullOrEmpty(hero.LastName));
 var heroesFilterString = string.Join(", ", heroesUnknownLastName);
 Console.WriteLine();
 Console.WriteLine("HEROES WHO HAS NOT LAST NAME");
 Console.WriteLine(heroesFilterString);
 
-static List<Hero> HeroesFilterWithoutDelegate(List<Hero> heroes, Func<Hero, bool> myMethodName)
+// with the generic delegate and function we can use infinity filter, for example
+var superHeroesWithFirstLetterW = GenericFilter(new[] { "Spiderman", "Batman", "HomeLander", "Deadpool", "Wondereoman" }, name => name.StartsWith("W")).ToList();
+var evenNumbers = GenericFilter(new[] { 1, 2, 3, 5, 6, 7, 8, 9, 23, 45 }, number => number % 2 == 0).ToList();
+
+// IEnumerable is just forward only and read only
+static IEnumerable<Hero> HeroesFilterWithoutDelegate(IEnumerable<Hero> heroes, Func<Hero, bool> myMethodName)
 {
     // Same function than FilterHeroesWhoCanFly/HeroesWhoLastNameIsUnknown
     // but converter to LINQ. Notice this is the group form, we can call the function "myMethodName"
@@ -30,9 +39,16 @@ static List<Hero> HeroesFilterWithoutDelegate(List<Hero> heroes, Func<Hero, bool
     return heroes.Where(myMethodName).ToList();
 }
 
-static List<Hero> HeroesFilterWithDelegate(List<Hero> heroes, Filter myMethodName)
+static IEnumerable<Hero> HeroesFilterWithDelegate(IEnumerable<Hero> heroes, Filter myMethodName)
 {
     return heroes.Where(hero => myMethodName(hero)).ToList();
+}
+
+
+
+static IEnumerable<T> GenericFilter<T>(IEnumerable<T> items, GenericFilter<T> genericFilter)
+{
+    return items.Where(item => genericFilter(item)).ToList();
 }
 
 
@@ -48,6 +64,31 @@ List<Hero> FilterHeroesWhoCanFly(List<Hero> heroes)
         }
     }
     return resultList;
+}
+
+// The same than below but using Yield,
+// yield is a keyword that works in any function that return an IEnumerable
+IEnumerable<Hero> FilterHeroesWhoCanFlyUsingYield(IEnumerable<Hero> heroes)
+{
+    foreach (var hero in heroes)
+    {
+        if (hero.CanFly)
+        {
+            yield return hero;
+        }
+    }
+}
+
+// Same function below but generic and GenericFilter as a parameter
+IEnumerable<T> GenericFilterHeroesWhoCanFlyUsingYield<T>(IEnumerable<T> items, GenericFilter<T> filter)
+{
+    foreach (var item in items)
+    {
+        if (filter(item))
+        {
+            yield return item;
+        }
+    }
 }
 
 List<Hero> HeroesWhoLastNameIsUnknown(List<Hero> heroes)
@@ -67,3 +108,6 @@ List<Hero> HeroesWhoLastNameIsUnknown(List<Hero> heroes)
 // Delegate is for a methods what Interface is for a class
 delegate bool Filter(Hero hero);
 record Hero(string FirstName, string LastName, string HeroName, bool CanFly);
+
+// Generic delegate
+delegate bool GenericFilter<T>(T hero);
